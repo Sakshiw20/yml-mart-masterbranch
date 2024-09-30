500e0aa287500d61a9bf429a31b0fbb88f12dc40
Ubuntu <ubuntu@ip-172-31-63-211.ec2.internal>
2024-09-30 20:17:26 +0000

    m

--- frontend/src/components/UploadProduct.js
+++ frontend/src/components/UploadProduct.js
@@ -36,7 +36,7 @@ const UploadProduct = ({
   const [openFullScreenImage, setOpenFullScreenImage] = useState(false);
   const [fullScreenImage, setFullScreenImage] = useState("");
   
-  // alert(authToken)
+
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setData((prev) => ({
